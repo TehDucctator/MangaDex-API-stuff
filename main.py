086f4b3234259503f3533f477c31d863c7265713
Ducc <ducky543210@gmail.com>
2022-05-19 19:57:04 -0400

    added menu, random, fixed getting cover art

--- main.py
+++ main.py
@@ -5,32 +5,49 @@ BASE_URL = "https://api.mangadex.org"
 
 # searches titles and returns a list of ids of the results
 def search(title: str):
-    encoded = title.replace(" ", "%20") # replaces spaces with %20 for URL
-    response = requests.get(BASE_URL + f"/manga?title={encoded}").json() # gets response in JSON
+    url_title = title.replace(" ", "%20") # replaces spaces with %20 for URL
+
+    response = requests.get(BASE_URL + f"/manga?title={url_title}").json() # gets response in JSON
     results = response["data"] # gets only the data 
 
     print(f"{'-'*25}\nRESULTS: ")
+
     ids = []
+
     for i, result in enumerate(results): # iterates through each result
         ids.append(result["id"]) # adds id to ids for easy access
 
         print(f"{i+1}: {list(result['attributes']['title'].values())[0]}") # prints title
 
+
     return ids
+
 
 # gets details about a title with its id
 def get_details(id: str):
     print('-'*25)
     print(f"URL: https://mangadex.org/title/{id}\n") # prints mangadex page
+
     response = requests.get(f"{BASE_URL}/manga/{id}?includes[]=cover_art").json() # Gets response in JSON
     results = response['data'] # gets only the data
 
-    print("Description:\n" + results['attributes']["description"]['en']) # prints description
+    print("TITLE: " + results['attributes']['title']['en'] + '\n')
+
+    # prints description
+    print("Description:")
+    print(results['attributes']["description"]['en'])
     print('-'*25)
+
 
     # gets cover art
     COVER_URL = "https://uploads.mangadex.org/covers/"
-    file_name = results['relationships'][2]['attributes']['fileName'] # gets file name (in relationships, index 2, attributes, fileName)
+
+    for i, relationship in enumerate(results['relationships']): # searches for cover art
+        if relationship['type'] == 'cover_art':
+            index = i
+            break
+
+    file_name = results['relationships'][index]['attributes']['fileName'] # gets file name
     cover_req = requests.get(f"{COVER_URL}{id}/{file_name}", stream=True)
 
     # displays cover art
@@ -38,6 +55,26 @@ def get_details(id: str):
     img.show()
 
 
-ids = search(input("What would you like to search?\n"))
-print(f"{'-'*25}")
-get_details(ids[int(input("Which one would you like to learn more about? (enter number):\n"))-1])
+def random():
+    response = requests.get(f"{BASE_URL}/manga/random?includes[]=cover_art").json() # requests random manga
+
+    id = response['data']['id'] # gets id
+    get_details(id) # prints details
+
+
+def main():
+    # menu
+    print("1. Search \n2. Random")
+    choice = int(input("Select an option: \n"))
+
+    if choice == 1:
+        ids = search(input("What would you like to search?\n"))
+        print(f"{'-'*25}")
+        get_details(ids[int(input("Which one would you like to learn more about? (enter number):\n"))-1])
+    
+    else:
+        random()
+
+
+if __name__ == '__main__':
+    main()
