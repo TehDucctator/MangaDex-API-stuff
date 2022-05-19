86b5a7bdef2d0df643bc3435778a0c84b20e2b3a
Ducc <ducky543210@gmail.com>
2022-05-18 20:05:25 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+from PIL import Image
+
+BASE_URL = "https://api.mangadex.org"
+
+# searches titles and returns a list of ids of the results
+def search(title: str):
+    encoded = title.replace(" ", "%20") # replaces spaces with %20 for URL
+    response = requests.get(BASE_URL + f"/manga?title={encoded}").json() # gets response in JSON
+    results = response["data"] # gets only the data 
+
+    ids = []
+    for i, result in enumerate(results): # iterates through each result
+        ids.append(result["id"]) # adds id to ids for easy access
+
+        print(f"{i+1}: {list(result['attributes']['title'].values())[0]}") # prints title
+
+    return ids
+
+# gets details about a title with its id
+def get_details(id: str):
+    print(f"https://mangadex.org/title/{id}") # prints mangadex page
+    response = requests.get(f"{BASE_URL}/manga/{id}?includes[]=cover_art").json() # Gets response in JSON
+    results = response['data'] # gets only the data
+
+    print("description:\n" + results['attributes']["description"]['en']) # prints description
+
+    # gets cover art
+    COVER_URL = "https://uploads.mangadex.org/covers/"
+    file_name = results['relationships'][2]['attributes']['fileName'] # gets file name (in relationships, index 2, attributes, fileName)
+    cover_req = requests.get(f"{COVER_URL}{id}/{file_name}", stream=True)
+
+    # displays cover art
+    img = Image.open(cover_req.raw)
+    img.show()
+
+
+ids = search(input("What would you like to search?\n"))
+get_details(ids[int(input("Which one would you like to learn more about? (enter number):\n"))-1])
